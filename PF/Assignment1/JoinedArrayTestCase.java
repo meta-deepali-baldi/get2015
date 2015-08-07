1dea3951fb7f02d07edbd7fc7c68b9cf58063c73
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-07 17:45:39 +0530

    Update JoinedArrayTestCase.java

--- PF/Assignment1/JoinedArrayTestCase.java
+++ PF/Assignment1/JoinedArrayTestCase.java
@@ -7,6 +7,14 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+/**
+ * @author Deepali
+ *Write a program to join two sorted arrays and generate third sorted array.
+ *int[] join(int a[], int asize, int b[], int bsize, int c[])
+ *asize is number of elements in array a , bsize is number of elements in array b
+ *Assume array c has sufficient space to store elements. Do not sort array c explicit.
+ *Write JUnit test cases using a method which takes two arrays as input and return true or false for asserting.
+ */
 
 public class JoinedArrayTestCase {
 
@@ -26,23 +34,30 @@ public class JoinedArrayTestCase {
 	public void tearDown() throws Exception {
 	}
 
+	
+	/**
+	 * JUnit test cases using a method which takes two arrays as input and return true or false for asserting.
+	 */
 	@Test
 	public void test() {
-		int array1[]={1,2,2,2,3,3};
+		int array1[]={1,2,2,2,3,3};//
 		int array2[]={1,4,7,9};
 		int array3[]=new int[array1.length+array2.length];
 		array3=Assignment6.join(array1,array1.length,array2,array2.length,array3);
-		int array4[]={1,1,2,2,2,3,3,4,7,9};
-		assertArrayEquals("correct",array4,array3);
+		int array4[]={1,1,2,2,2,3,3,4,7,9};//expected array
+		assertArrayEquals("True",array4,array3);
 	}
 	
+	/**
+	 * JUnit test cases using a method which takes two arrays as input and return true or false for asserting.
+	 */
 	@Test
 	public void test1() {
 		int array1[]={1,3,4,7,9};
 		int array2[]={2,7,9};
 		int array3[]=new int[array1.length+array2.length];
 		array3=Assignment6.join(array1,array1.length,array2,array2.length,array3);
-		int array4[]={1,2,3,4,7,7,9,9};
-		assertArrayEquals("correct",array4,array3);
+		int array4[]={1,2,3,4,7,7,9,9};//expected array
+		assertArrayEquals("True",array4,array3);
 	}
 }
