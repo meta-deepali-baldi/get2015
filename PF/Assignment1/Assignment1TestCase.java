9dd8bacd5591a23465140b54d8dc2ddf677992a8
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-07 15:45:28 +0530

    Update Assignment1TestCase.java

--- PF/Assignment1/Assignment1TestCase.java
+++ PF/Assignment1/Assignment1TestCase.java
@@ -8,6 +8,22 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
+/**
+ * @author Deepali
+ *
+ *Write a function (int convertBinaryToOctal(int n) )to convert binary numbers to octal eg.
+
+	      Input:
+
+Binary Number: 110101
+
+	     Output:
+
+Octal Number:  65
+
+Also write JUnit test cases which takes above input and assert octal number.
+ */
+
 public class Assignment1TestCase {
 	ConvertBinaryToOctal c1=new ConvertBinaryToOctal();
 	@BeforeClass
@@ -28,16 +44,30 @@ public class Assignment1TestCase {
 	//nothing to tear
 	}
 
+	
+	/**
+	 * JUnit test cases which takes above input and assert octal number for ConvertBinaryToOctal function
+	 */
 	@Test
 	public void test() {
 		
 		int octal=c1.convertBinaryToOctal(11011);
 		assertEquals("correct",33,octal);
 	}
+	
+	/**
+	 * JUnit test cases which takes above input and assert octal number for ConvertBinaryToOctal function
+	 */
+	@Test
 	public void test1() {
 		int octal=c1.convertBinaryToOctal(110101);
 		assertEquals("correct",65,octal);
 	}
+	
+	/**
+	 * JUnit test cases which takes above input and assert octal number for ConvertBinaryToOctal function
+	 */
+	@Test
 	public void test2() {
 		int octal=c1.convertBinaryToOctal(110);
 		assertEquals("correct",6,octal);
