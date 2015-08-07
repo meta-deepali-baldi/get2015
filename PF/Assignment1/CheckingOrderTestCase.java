99967821eef5611cafd57ba1eec180ca217a58c6
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-07 17:35:56 +0530

    Update CheckingOrderTestCase.java

--- PF/Assignment1/CheckingOrderTestCase.java
+++ PF/Assignment1/CheckingOrderTestCase.java
@@ -8,6 +8,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author Deepali
+ *Write a function to check given array is in sorted order or not. Function should return 1 if array is in ascending order, 2 if array is in descending order, 0 if array is not in sorted
+ *Use this function and write a program to display appropriate message based on return value of above function.
+ *Also write JUnit test cases which takes an integer array as input and assert return value.
+ *Write Junit Test cases for all above 3 cases.
+ */
 public class CheckingOrderTestCase {
 
 	@BeforeClass
@@ -26,25 +33,38 @@ public class CheckingOrderTestCase {
 	public void tearDown() throws Exception {
 	}
 
+	
+	/**
+	 * Junit Test cases for Ascending order on checkingOrderOfArray Method
+	 */
 	@Test
 	public void test() {
 		int array1[]={1,2,2,2,3,3};
-		int order=Assignment5.checkingorderofarray(array1);
+		int order=Assignment5.checkingOrderOfArray(array1);
 	
 		assertEquals("Ascending order",1,order);
 	}
 	
+
+	/**
+	 * Junit Test cases for Not sorted order on checkingOrderOfArray Method
+	 */
 	@Test
 	public void test1() {
 		int array1[]={1,4,6,3,7,8,9};
-		int order=Assignment5.checkingorderofarray(array1);
+		int order=Assignment5.checkingOrderOfArray(array1);
 	
 		assertEquals("Not sorted",0,order);
 	}
+	
+
+	/**
+	 * Junit Test cases for Descending order on checkingOrderOfArray Method
+	 */
 	@Test
 	public void test2() {
 		int array1[]={9,6,4,2,0};
-		int order=Assignment5.checkingorderofarray(array1);
+		int order=Assignment5.checkingOrderOfArray(array1);
 	
 		assertEquals("Descending order",2,order);
 	}
