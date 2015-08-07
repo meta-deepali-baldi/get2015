1a073255333299c63de454ac5f47e1d300cc6d4a
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-07 14:49:47 +0530

    Update Assignment22TestCase.java

--- PF/Assignment2/Assignment22TestCase.java
+++ PF/Assignment2/Assignment22TestCase.java
@@ -8,6 +8,21 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+/**
+ * @author Deepali
+ * 1 2 3 4 5 
+     1 2 3 4 
+       1 2 3 
+         1 2 
+           1
+Write a program to display above structure using modular approach. 
+Divide whole problem in 3 parts as per describe in the presentation.
+1. spaces(row, n): return the spaces string
+2. numbers(row, n): return the number string
+3. print whole pyramid : It will return the array of string by using above 2 functions.
+Also write JUnit test cases for each modules.
+ */
 public class Assignment22TestCase {
 	 Assignment22 a1=new Assignment22();
 		@BeforeClass
@@ -26,18 +41,29 @@ public class Assignment22TestCase {
 		public void tearDown() throws Exception {
 		}
 
+		/**
+		 * JUnit test case for spaces(row, n): return the spaces string
+		 */
 		@Test
 		public void test1() {
 			
 			String s1=a1.spaces(4,5);
 			assertEquals("Correct","   ",s1);
 		}
+		
+		/**
+		 * JUnit test case for numbers(row, n): return the number string
+		 */
 		@Test
 		public void test2() {
 			
 				String s1=a1.number(4,5);
 				assertEquals("Correct","12",s1);
 		}
+		
+		/**
+		 * JUnit test case for printWholePyramid : It will return the array of string by using above 2 functions
+		 */
 		@Test
 		public void test3() {
 			String expected[]={"12345"," 1234","  123","   12","    1"};
