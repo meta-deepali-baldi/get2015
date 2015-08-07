4db313ed8e3959d28b2b1a9c690af532d8470b66
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-07 17:00:48 +0530

    Update LongestSequenceTestCase.java

--- PF/Assignment1/LongestSequenceTestCase.java
+++ PF/Assignment1/LongestSequenceTestCase.java
@@ -8,6 +8,16 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author Deepali
+ *Write a program to find longest increasing sequence from an array of n integer values
+ *Input:
+ *Given Array: [1,2,3,2,3,4,5,3,4,2,2,3,4,5,6,7,8,1,2,4,5,6,7,8,9]
+ *Output:
+ *Resultant Array: [1, 2,4,5,6,7,8,9]
+ *method name : int[] longestSequence(int input[])
+ *Write JUnit test cases which will assert the array return from the above method and the expected array.
+*/
 public class LongestSequenceTestCase {
 
 	@BeforeClass
@@ -24,24 +34,22 @@ public class LongestSequenceTestCase {
 		
 	}
 
+	
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception {//initializing instance variable here
 		Assignment3.count=0;
 		Assignment3.count2=0;
 		Assignment3.count2=0;
 		Assignment3.greater=0;
 	}
 
-	@Test
-	public void test() {
-		int array1[]={1,1,2,3,4,1};
-		int[] array2=Assignment2.removeDuplicate(array1);
-		int array3[]={1,2,3,4,0,0};
-		assertArrayEquals("correct",array3,array2);
-	}
+	
+	/**
+	 * JUnit test cases which will assert the array returned and the expected array for longestSequence
+	 */
 	public void test1() {
 		int array1[]={1,2,3,2,3,4,5,3,4,2,2,3,4,5,6,7,8,1,2,4,5,6,7,8,9};
-		int[] array2=Assignment2.removeDuplicate(array1);
+		int[] array2=Assignment3.longestSequence(array1);
 		int array3[]={1,2,4,5,6,7,8,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 		assertArrayEquals("correct",array3,array2);
 	}
