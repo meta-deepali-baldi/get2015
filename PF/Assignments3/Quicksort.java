12afc45fc9ca2683507f2cc8da8656c680984785
Deepali Baldi <deepali.baldi@metacube.com>
2015-08-11 14:46:43 +0530

    Update Quicksort.java

--- PF/Assignments3/Quicksort.java
+++ PF/Assignments3/Quicksort.java
@@ -10,14 +10,16 @@ import java.util.Scanner;
 
 /**
  * @author Deepali
+ * array :Denote an integer array to be sorted
  */
 public class Quicksort {
-int array[];
+private int array[];
 	public static void main(String[] args) {
 		int size,temp;
+		int finalArr[];
 		Quicksort quicksort=new Quicksort();
 		Scanner sc = new Scanner(System.in);
-		do {
+		do {//Continues Looping
 			System.out.println("Enter size of the array");
 			size = sc.nextInt();
 			quicksort.array = new int[size];
@@ -25,17 +27,13 @@ int array[];
 			for (int i = 0; i < size; i++) {
 				quicksort.array[i] = sc.nextInt();
 			}
-			if (size > 0) {// checking if elements are their in array or not
-				int finalArr[]  = sortArray(quicksort.array, 0, size - 1);
+		
+				finalArr= sortArray(quicksort.array, 0, size - 1);
 
 				System.out.println("\nElements after sorting ");
 				for (int i = 0; i < size; i++) {
 					System.out.print(finalArr[i] + " ");
 					System.out.println();
-				}
-			}
-			 else
-				System.out.println("Enter Correct value");
 
 			System.out.println("Press 1 if you want to exit");
 			System.out.println("Press 0 if you want to continue");
@@ -56,10 +54,10 @@ int array[];
  * @return--sorted array
  */
 public static int[] sortArray(int arr[], int low, int high) {
-	if ((low == 0) && (high == 0)) {
+	if ((low == 0) && (high == 0)) {// checking if elements are their in array or not
 		return arr;
 	} else
-		quickSort(arr, low, high);
+		quickSort(arr, low, high);//Recursive Function
 	return arr;
 }
 
