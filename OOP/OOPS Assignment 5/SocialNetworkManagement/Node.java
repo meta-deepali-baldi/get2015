986f1b6b29b3dc2bd88ce6fe34b79df8a0bca4f5
Deepali Baldi <deepalibaldi@metacube.com>
2015-08-25 01:17:44 +0530

    Completed

---
+++ OOP/OOPS Assignment 5/SocialNetworkManagement/Node.java
@@ -0,0 +1,13 @@
+package SocialNetworkManagement;
+
+import java.math.BigInteger;
+
+/**
+ * @author Deepali
+ *
+ */
+public interface Node {//Interface implemented in Entity class
+	public String getName() ;
+	public BigInteger getPhone();
+	public String getEmail();
+}
