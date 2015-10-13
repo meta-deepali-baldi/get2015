dcef4b6aa4e6d128123263ba15162a27ba35d077
Deepali Baldi <deepali.baldi@metacube.com>
2015-10-14 01:55:22 +0530

    Update Registeration.java

--- Web/JSP-Servlet-Assignment-1/Assignment2/src/com/servlet/Registeration.java
+++ Web/JSP-Servlet-Assignment-1/Assignment2/src/com/servlet/Registeration.java
@@ -1,11 +1,18 @@
+/**
+Write a web application that:
+Contains a user registration information form that requests a 	servlet.
+Contains a servlet that accepts the request sent by client, validates the form data and dispatches the validation result to the other servlet and that servlet displays the validation result.
+
+
+Note: The registration form can be the form created in previous assignment and the information sent must be validated on the server side.
+ */
+
 package com.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * Deepali
+ * 
  * Servlet implementation class Registeration
  */
 @WebServlet("/Registeration")
@@ -27,91 +36,81 @@ public class Registeration extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw = response.getWriter();
 		boolean flag = true;
-		String result="";
+		String result = "";
 		response.setContentType("text/html");
 		pw.println("This is Registeration page.<br>");
 
-		if (request.getParameter("first_name")=="") {
+		if (request.getParameter("first_name") == "") {
 			flag = false;
-			result+="First Name not Filled<br>";
+			result += "First Name not Filled<br>";
 		}
-		if (request.getParameter("last_name")=="") {
+		if (request.getParameter("last_name") == "") {
 			flag = false;
-			result+="Last name not Filled<br>";
+			result += "Last name not Filled<br>";
 		}
 		if (request.getParameter("gender").equals("-Select One-")) {
 			flag = false;
-			result+="Gender is not selected<br>";
+			result += "Gender is not selected<br>";
 		}
-		if (request.getParameter("dob")=="") {
+		if (request.getParameter("dob") == "") {
 			flag = false;
-			result+="Date of Birth Not Selected<br>";
+			result += "Date of Birth Not Selected<br>";
 		}
 		if (request.getParameter("country").equals("-Select One-")) {
 			flag = false;
-			result+="country is not selected<br>";
+			result += "country is not selected<br>";
 		}
-		if (request.getParameter("email")=="") {
+		if (request.getParameter("email") == "") {
 			flag = false;
-			result+="Email not specified<br>";
+			result += "Email not specified<br>";
 		}
-		if (request.getParameter("pwd")=="") {
+		if (request.getParameter("pwd") == "") {
 			flag = false;
-			result+="Password not specified<br>";
+			result += "Password not specified<br>";
 		}
-		if(request.getParameter("pwd").length() < 8 )
-		{
-			result+="Password Length should be greater than 8 <br>";
-		     flag=false;
-		}
-		if (request.getParameter("cpwd")=="") {
+		if (request.getParameter("pwd").length() < 8) {
+			result += "Password Length should be greater than 8 <br>";
 			flag = false;
-			result+="Reenter password not specified<br>";
 		}
-		if(request.getParameter("cpwd").length() < 8 )
-		{
-			result+="Reenter Password Length should be greater than 8 <br>";
-		     flag=false;
+		if (request.getParameter("cpwd") == "") {
+			flag = false;
+			result += "Reenter password not specified<br>";
+		}
+		if (request.getParameter("cpwd").length() < 8) {
+			result += "Reenter Password Length should be greater than 8 <br>";
+			flag = false;
 		}
 		if (!request.getParameter("pwd").equals(request.getParameter("cpwd"))) {
 			flag = false;
-			result+="Password and Reentered Password does not match<br>";
+			result += "Password and Reentered Password does not match<br>";
 		}
 		if (request.getParameter("questionFirst").equals("-Select One-")) {
 			flag = false;
-			result+="First Question is not Selected<br>";
+			result += "First Question is not Selected<br>";
 		}
-		if (request.getParameter("answerFirst")=="") {
+		if (request.getParameter("answerFirst") == "") {
 			flag = false;
-			result+="Answer of first Question not specified<br>";
+			result += "Answer of first Question not specified<br>";
 		}
-		
+
 		if (request.getParameter("questionSecond").equals("-Select One-")) {
 			flag = false;
-			result+="Second Question is not Selected<br>";
+			result += "Second Question is not Selected<br>";
 		}
-		if (request.getParameter("answerSecond")=="") {
+		if (request.getParameter("answerSecond") == "") {
 			flag = false;
-			result+="Answer of first Question not specified<br>";
+			result += "Answer of first Question not specified<br>";
 		}
-		
+
 		if (flag == true) {
-			result+="Form is validated at server side<br>";
+			result += "Form is validated at server side<br>";
 
 		}
+
 		request.setAttribute("result", result);
 		RequestDispatcher rd = request.getRequestDispatcher("DirectedResponse");
 		rd.forward(request, response);
 
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
 	}
 
 }
