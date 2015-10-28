42374380b48ecdcbec96957fd37dd4c63754cd9c
Deepali Baldi <deepali.baldi@metacube.com>
2015-10-28 17:46:02 +0530

    Update AdminHome.java

--- CASESTUDY/Case Study Assignment-4/CaseStudyAssignment-4/src/com/metacube/carportal/controller/AdminHome.java
+++ CASESTUDY/Case Study Assignment-4/CaseStudyAssignment-4/src/com/metacube/carportal/controller/AdminHome.java
@@ -18,11 +18,9 @@ Login for administrator
 Create/Edit only for administrator
  */
 
-
 package com.metacube.carportal.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -34,9 +32,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.metacube.carportal.db.helper.CarPortalDao;
-import com.metacube.carportal.dbconfig.ConnectionFactory;
-import com.metacube.carportal.exception.CarDekhoException;
 import com.metacube.carportal.model.Car;
+import com.metacube.carportal.service.ConnectionForServlet;
 
 /**
  * Deepali
@@ -48,43 +45,39 @@ public class AdminHome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Car> carList=null;
-		//getting username and password from request
-		String username=request.getParameter("username");
-		if(request.getParameter("username")==null){
-			//setting username to session
-		HttpSession session=request.getSession(true);
-		username=(String) session.getAttribute("username");
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		List<Car> carList = null;
+		// getting username and password from request
+		String username = request.getParameter("username");
+		if (request.getParameter("username") == null) {
+			// setting username to session
+			HttpSession session = request.getSession(true);
+			username = (String) session.getAttribute("username");
 		}
-		try {//getting connection
-			Connection connection=ConnectionFactory.getConnection();
-			if(connection==null){
-				connection=new ConnectionFactory().createConnection();
-				new CarPortalDao(connection);
-			}
-			//getting list of all cars
-			carList = CarPortalDao.selectListOfCar(connection);
-			
-		} catch (CarDekhoException e) {
-			e.printStackTrace();
-		}
-		//Request fowarded to Home page of admin
-		request.setAttribute("username",username);
-		request.setAttribute("carList",carList);
+
+		// getting list of all cars
+		carList = CarPortalDao.selectListOfCar(ConnectionForServlet
+				.getConnectionForServlet());
+		// Request fowarded to Home page of admin
+		request.setAttribute("username", username);
+		request.setAttribute("carList", carList);
 		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
 		rd.forward(request, response);
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
-		
+
 	}
 
 }
